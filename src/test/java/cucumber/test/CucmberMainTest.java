799cf9292a5500553d0f41b7a0a6ebf40e1f2999
FerPa <fpalazuelo@gmail.com>
2017-04-03 23:37:17 +0200

    cucumber 2

--- src/test/java/cucumber/test/CucmberMainTest.java
+++ src/test/java/cucumber/test/CucmberMainTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/cucumber/resources")
+@CucumberOptions(features = "src/test/java/cucumber/resources/features",glue = "steps" )
 public class CucmberMainTest {
 
 }
